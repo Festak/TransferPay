6c27cb7286841f6b1b95a020f78318b2e846110a
Fetskovich <e.fetskovich@itransition.com>
2017-11-14 18:34:41 +0300

    Implemented delete all transactions.

--- app-transferpay/src/main/java/com/transfer/pay/ui/fragments/transactionhistory/TransactionHistoryPresenter.java
+++ app-transferpay/src/main/java/com/transfer/pay/ui/fragments/transactionhistory/TransactionHistoryPresenter.java
@@ -1,15 +1,19 @@
 package com.transfer.pay.ui.fragments.transactionhistory;
 
+import android.util.Log;
+
 import com.istatkevich.cmvp.core.viewmodel.EmptyViewModel;
 import com.transfer.pay.UserManager;
 import com.transfer.pay.databinding.TransactionHistoryBinding;
+import com.transfer.pay.models.User;
+import com.transfer.pay.ormlite.ORMLiteFactcory;
 import com.transfer.pay.ui.TransferPayBasePresenter;
 
-/**
- * Created by i.statkevich on 5/25/17.
- */
+import java.sql.SQLException;
 
 public class TransactionHistoryPresenter extends TransferPayBasePresenter<EmptyViewModel, TransactionHistoryViewHelper> {
+
+    private static final String TAG = "TransactionHistoryPrese";
 
     @Override
     protected void onPresenterReady() {
@@ -18,6 +22,19 @@ public class TransactionHistoryPresenter extends TransferPayBasePresenter<EmptyV
 
     public void bindVariables(TransactionHistoryBinding binding) {
         binding.setPresenter(this);
+    }
+
+    public void onAllTransactionDeleteClick() {
+        Log.i(TAG, "onAllTransactionDeleteClick: ");
+        User user = UserManager.getInstance().getUser();
+        user.getTransactions().clear();
+        try {
+            ORMLiteFactcory.getHelper().getUserDao().update(user);
+            UserManager.getInstance().updateUser();
+            getViewHelper().updateData(UserManager.getInstance().getUser().getTransactions());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
