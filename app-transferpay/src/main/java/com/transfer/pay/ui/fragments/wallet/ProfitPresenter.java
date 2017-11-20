311eb81de48966645e46e188d24cad0b64cdfd70
Fetskovich <e.fetskovich@itransition.com>
2017-11-20 14:32:06 +0300

    Added graph

--- app-transferpay/src/main/java/com/transfer/pay/ui/fragments/wallet/ProfitPresenter.java
+++ app-transferpay/src/main/java/com/transfer/pay/ui/fragments/wallet/ProfitPresenter.java
@@ -1,12 +1,16 @@
 package com.transfer.pay.ui.fragments.wallet;
 
 import com.istatkevich.cmvp.core.viewmodel.EmptyViewModel;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.transfer.pay.databinding.WalletBinding;
 import com.transfer.pay.models.TransactionFeeProfit;
 import com.transfer.pay.ormlite.ORMLiteFactcory;
 import com.transfer.pay.ui.TransferPayBasePresenter;
 
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,19 +27,40 @@ public class ProfitPresenter extends TransferPayBasePresenter<EmptyViewModel, Pr
         binding.setPresenter(this);
     }
 
+    public LineGraphSeries<DataPoint> getListOfPoints() {
+        List<TransactionFeeProfit> list = getListOfProfits();
+        Calendar calendar = Calendar.getInstance();
+        DataPoint[] dataPoints = new DataPoint[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            TransactionFeeProfit feeProfit = list.get(i);
+            calendar.setTimeInMillis(feeProfit.getTimestamp());
+            Date d = calendar.getTime();
+            DataPoint dp = new DataPoint(d, feeProfit.getFeeProfit());
+            dataPoints[i] = dp;
+        }
+
+        return new LineGraphSeries<>(dataPoints);
+    }
+
     public double calculateTotalFeeCount() {
         double totalFee = 0;
+        List<TransactionFeeProfit> list = getListOfProfits();
+
+        for (TransactionFeeProfit transactionFeeProfit : list) {
+            totalFee += transactionFeeProfit.getFeeProfit();
+        }
+        return totalFee;
+    }
+
+    private List<TransactionFeeProfit> getListOfProfits() {
         List<TransactionFeeProfit> list = new LinkedList<>();
         try {
             list = ORMLiteFactcory.getHelper().getTransactionFeeProfitsDao().getAllTransactions();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        for (TransactionFeeProfit transactionFeeProfit : list) {
-            totalFee += transactionFeeProfit.getFeeProfit();
-        }
-        return totalFee;
+        return list;
     }
 
 }
--- app-transferpay/src/main/java/com/transfer/pay/ui/fragments/wallet/ProfitViewHelper.java
+++ app-transferpay/src/main/java/com/transfer/pay/ui/fragments/wallet/ProfitViewHelper.java
@@ -1,6 +1,10 @@
 package com.transfer.pay.ui.fragments.wallet;
 
 import com.istatkevich.cmvp.core.viewhelper.ViewHelper;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.transfer.pay.R;
 import com.transfer.pay.databinding.WalletBinding;
 
@@ -14,6 +18,31 @@ public class ProfitViewHelper extends ViewHelper<ProfitPresenter, WalletBinding>
     @Override
     protected void onInitView() {
         super.onInitView();
+        initGraph();
+    }
+
+    private void initGraph() {
+        LineGraphSeries<DataPoint> points = getPresenter().getListOfPoints();
+
+        GraphView graph = getBinding().graph;
+        points.setTitle("Profit");
+        points.setDrawDataPoints(true);
+        points.setAnimated(true);
+        graph.addSeries(points);
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(graph.getContext()));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(2);
+
+        graph.getViewport().setMinX(points.getLowestValueX());
+        graph.getViewport().setMaxX(points.getHighestValueX());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
     }
 
     @Override
